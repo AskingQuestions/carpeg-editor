d7765ad8ea1ba8011db6679b3b9d28237ba9c409
AskingQuestions <tiger11451145@gmail.com>
2018-05-19 10:27:23 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# carpeg-editor
+# Carpeg Editor
+
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/Echorial/carpeg.svg?style=flat-square)
+![GitHub last commit](https://img.shields.io/github/last-commit/Echorial/carpeg.svg?style=flat-square)
+![license](https://img.shields.io/github/license/Echorial/carpeg.svg?style=flat-square)
+
 A simple web based Carpeg editor for testing grammar.
+
+[Demo]()
---
+++ worker.js
@@ -0,0 +1,33 @@
+importScripts("carbonite.js", "carpeg.js");
+
+onmessage = (event) => {
+	postMessage(buildGrammar(event.data));
+};
+
+function buildCarbonite(str, platform) {
+	let c = new Carbonite.Compiler();
+	c.addNativeLibrary();
+
+	let main = c.addSource("main", str);
+	main.file = "main";
+	main.process();
+
+	c.build(platform || "javascript.source.memory", {});
+
+	if (c.status.hadError)
+		console.error(c.status.firstError.stringify());
+	
+	return {code: c.rawOutput, error: c.status.hadError ? c.status.firstError.stringify() : false};
+}
+
+function buildGrammar(str) {
+	let grammar = new Carpeg.grammar(str);
+	grammar.parserClass = "DemoParser";
+	try {
+		var carbonOutput = grammar.generate();
+	}catch(e) {
+		return {code: "", error: e.toString()}
+	}
+	
+	return buildCarbonite(carbonOutput);
+}
